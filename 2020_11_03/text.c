506ddc5ea68a83d51026a317b9d56e0a267b11fb
Mercury <mercuryge@163.com>
2020-11-03 00:08:35 +0800

    第一次提交代码

---
+++ 2020_11_03/text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+//int main()
+//{
+//	//int arr1[10] = { 0 };
+//	//int arr2[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	//char ch[10] = "hello bit";
+//	//printf("%d\n", sizeof(ch));
+//	//printf("%d\n", strlen(ch));
+//	system("pause");
+//    return 0;
+//}
