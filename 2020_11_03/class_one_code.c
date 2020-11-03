6998895f4524daea9c1af4f95467e77ed8e41f82
Mercury <mercuryge@163.com>
2020-11-03 18:31:01 +0800

    联系代码“比较两个数大小”

---
+++ 2020_11_03/class_one_code.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+//int main()
+//{
+//	//int arr1[10] = { 0 };
+//	//int arr2[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	//char ch[10] = "hello bit";
+//	//printf("%d\n", sizeof(ch));
+//	//printf("%d\n", strlen(ch));
+//	system("pause");
+//    return 0;
+//}
+
+//比较两个数大小
+//int compare(int x, int y)
+//{
+//	int z = 0;
+//	if (x > y)
+//	{
+//		z = x;
+//	}
+//	else if (x < y)
+//	{
+//		z = y;
+//	}
+//	else
+//	{
+//	    char arr[]="Two numbers are equal";
+//	}
+//	return z;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("输入两个操作数：");
+//	scanf("%d %d", &num1, &num2);
+//	larger_number = compare(num1, num2);
+//	printf("larger number = %d\n", larger_number);
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	int num1 = 1;
+	int num2 = 1;
+	if (num1 = num2)
+	{
+		printf("两数相等\n");
+	}
+	else if (num1 > num2)
+	{
+		printf("较大的数字为：%d\n", num1)
+	}
+	else
+	{
+		printf("较大的数字为：%d\n", num2);
+	}
+	system("pause");
+	return 0;
+}
